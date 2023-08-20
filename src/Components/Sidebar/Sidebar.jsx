5a97425f9eeb434829784d08de9b1d61f16f854d
nsohpeter <apiahpetertoch@gmail.com>
2023-08-20 11:25:02 -1100

    update

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
 
         <ul className="other-links">
           <Link
-            to="/product"
+            to="/products"
             style={{ textDecoration: "none" }}
             onClick={() => dispatch(closeSidebar())}
           >
