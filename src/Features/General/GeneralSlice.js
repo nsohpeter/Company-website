481ea57055f9053a3e54428aa31bdbc3ac119a1a
nsohpeter <apiahpetertoch@gmail.com>
2023-09-27 23:04:32 -1100

    redux slice

--- src/Features/General/GeneralSlice.js
+++ src/Features/General/GeneralSlice.js
@@ -1,24 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
+import { sublinks } from '../../Utils/Data'
 
 const initialState = {
-  isSidebarOpen: false,
-};
+    isSidebarOpen: false,
+    linksItem: sublinks,
+    isSubmenuOpen: false,
+    location: {},
+    page: { page: "", links: [] }
+
+}
 
 export const GeneralSlice = createSlice({
-  name: "General",
-  initialState,
-  reducers: {
-    //Sidebar reducers
-    openSidebar: (State) => {
-      State.isSidebarOpen = true;
-    },
-    closeSidebar: (State) => {
-      State.isSidebarOpen = false;
-    },
-  },
-});
+    name: 'General',
+    initialState,
+    reducers: {
+        //Sidebar reducers
+        openSidebar: (State) => {
+            State.isSidebarOpen = true
+        },
+        closeSidebar: (State) => {
+            State.isSidebarOpen = false
+        },
+        //submenu reducers
+        openSubmenu: (State, action) => {
+
+            const { page, location } = action.payload;
+            const { center, bottom } = location;
+
+            const pageObj = State.linksItem.find((link) => link.page === page);
+
+            if (page) {
+                State.page = pageObj;
+            }
+            State.location = { center, bottom };
+
+            State.isSubmenuOpen = true
+
+        },
+        closeSubmenu: (State) => {
+            State.isSubmenuOpen = false
+        },
+
+    }
+})
+
 
 // Action creators are generated for each case reducer function
-export const { openSidebar, closeSidebar } = GeneralSlice.actions;
+export const { openSidebar, closeSidebar, openSubmenu, closeSubmenu } = GeneralSlice.actions
 
-export default GeneralSlice.reducer;
+export default GeneralSlice.reducer
