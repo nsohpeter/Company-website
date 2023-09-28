658f23f71635eb89a8b84d11d1aa070229f6cd77
nsohpeter <apiahpetertoch@gmail.com>
2023-09-28 00:08:28 -1100

    donation

---
+++ src/Pages/Donations/Donation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Donation.css";
+
+const Donation = () => {
+  return (
+    <div className="main-donation-conatainer">
+      <div className="banner-container">
+        <div className="text">
+          <h3>Your Donations will touch a life</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Donation;
