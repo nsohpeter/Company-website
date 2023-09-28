92c3f6a174bfa6683de24cbc07347fc61363e9e8
nsohpeter <apiahpetertoch@gmail.com>
2023-09-27 23:29:36 -1100

    sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
+import { FaChevronDown } from "react-icons/fa";
 
 import { closeSidebar } from "../../Features/General/GeneralSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { sublinks } from "../../Utils/Data";
 
 import { FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.General);
+  const [expandedPage, setExpandedPage] = useState(null);
+
   const dispatch = useDispatch();
 
+  const togglePage = (pageIndex) => {
+    setExpandedPage((prevExpandedPage) =>
+      prevExpandedPage === pageIndex ? null : pageIndex
+    );
+  };
   return (
     <section
       className={`${
@@ -22,23 +31,57 @@ const Sidebar = () => {
           <FaTimes className="close-icon" />
         </button>
 
+        <ul className="sidebar-links">
+          {sublinks.map((item, index) => {
+            const { links, page } = item;
+            const isPageExpanded = expandedPage === index;
+            return (
+              <li key={index} className="dropdowns">
+                <button className="dropdown-btn">
+                  {page}
+                  <FaChevronDown
+                    className="chevron-icon"
+                    onClick={() => togglePage(index)}
+                  />
+                </button>
+                {isPageExpanded && (
+                  <ul className="nested-sidebar-links">
+                    {links.map((link, index) => {
+                      const { label, url } = link;
+                      return (
+                        <li key={index} className="nested-subLinks">
+                          <Link
+                            to={url}
+                            onClick={() => dispatch(closeSidebar())}
+                          >
+                            {label}
+                          </Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
+              </li>
+            );
+          })}
+        </ul>
         <ul className="other-links">
           <Link
-            to="/products"
+            to="/volunteer"
             style={{ textDecoration: "none" }}
             onClick={() => dispatch(closeSidebar())}
           >
             <li className="other-list">
-              <span>PRODUCTS</span>
+              <span>volunteer</span>
             </li>
           </Link>
           <Link
-            to="/service"
+            to="/donation"
             style={{ textDecoration: "none" }}
             onClick={() => dispatch(closeSidebar())}
           >
             <li className="other-list">
-              <span>SERVICES</span>
+              <span>Donation</span>
             </li>
           </Link>
           <Link
@@ -47,7 +90,7 @@ const Sidebar = () => {
             onClick={() => dispatch(closeSidebar())}
           >
             <li className="other-list">
-              <span>ABOUT US</span>
+              <span>About us</span>
             </li>
           </Link>
           <Link
@@ -56,7 +99,7 @@ const Sidebar = () => {
             onClick={() => dispatch(closeSidebar())}
           >
             <li className="other-list">
-              <span>CONTACT US</span>
+              <span>contact</span>
             </li>
           </Link>
         </ul>
